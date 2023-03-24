b5d48a61ce56c5ad18beccfcdf44224623916dad
Alex Miserandino <alexmiserandino@Alexs-MacBook-Air.local>
2023-03-23 23:37:15 -0700

    redesigned  App

--- src/slider.js
+++ src/slider.js
@@ -2,10 +2,12 @@ import './index.css';
 
 const Slider = ({ volume, setVolume }) => {
   return (
+  <div>
     <div id="slide-containter">
-      <input type="range" min="0" max="100"  onChange={(e) => setVolume(e.target.value/100)}></input>
-      {`Volume: ${volume * 100}`}
+      <input type="range" min="0" max="100" step="1" onChange={(e) => setVolume(e.target.value/100)}></input>  
     </div>
+    <div id="volume">{Math.round(volume*100)}</div>
+  </div>
   )
 }
 export default Slider
