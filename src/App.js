f5bc29f225fd368b4ce76de6afb43b4033e0fb27
Alex Miserandino <alexmiserandino@Alexs-MacBook-Air.local>
2023-03-25 08:40:52 -0700

    refactored App component

--- src/App.js
+++ src/App.js
@@ -47,13 +47,13 @@ const getKeyDown = (e) => {
     getDisplay("Kick");
   }
   if (e.key.toLowerCase() === "c") {
-    playKeyDown("C")
+    playKeyDown("C");
     getDisplay("Closed HH");
   }
 }
 
-const playKeyDown = (id) => {
-  let sound = document.getElementById(id);
+const playKeyDown = (str) => {
+  let sound = document.getElementById(str);
   sound.volume = volume;
   sound.play();
 };
@@ -62,82 +62,43 @@ const getDisplay = (str) => {
    setDisplay(str)
 };
 
-const handleClick = (e) => {
-  if (e.currentTarget.id === "heater1") {
-     let sound = document.getElementById("Q");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Heater 1"); 
-   }
-  if (e.currentTarget.id === "heater2") {
-     let sound = document.getElementById("W");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Heater 2");
-   }
-  if (e.currentTarget.id === "heater3") {
-     let sound = document.getElementById("E");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Heater 3");
-  }
-  if (e.currentTarget.id === "heater4") {
-     let sound = document.getElementById("A");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Heater 4");
-  }
-  if (e.currentTarget.id === "clap") {
-     let sound = document.getElementById("S");
-     sound.volume = volume;
-     sound.play();    
-     getDisplay("Clap");
-  }
-  if (e.currentTarget.id === "open-HH") {
-     let sound = document.getElementById("D");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Open HH");
-  }
-  if (e.currentTarget.id === "kick-n-hat") {
-     let sound = document.getElementById("Z");
-     sound.volume = volume;
-     sound.play();
-     getDisplay("Kick n Hat");
-  }
-  if (e.currentTarget.id === "kick") {
-    let sound = document.getElementById("X");
-    sound.volume = volume;
-    sound.play();
-    getDisplay("Kick");
-  }
-  if (e.currentTarget.id === "closed-HH") {
-    let sound = document.getElementById("C");
-    sound.volume = volume;
-    sound.play()
-    getDisplay("Closed HH");
-  }
+const handleClick = (e, str) => {
+   let sound = document.getElementById(str);
+   sound.volume = volume;
+   sound.play();
+   getDisplay(e.currentTarget.id); 
 }
 
   return (
     <div className="App" id='drum-machine'>
       <div className='container' id='display'> 
          <div id='description'>{display}</div>
-               <Slider volume={volume} setVolume={setVolume}/> 
+           <Slider volume={volume} setVolume={setVolume}/> 
          <div className='display-container'>
-           <button className='drum-pad' id='heater1' type="button" onClick={handleClick}><audio src={src1} className="clip" id="Q"></audio>Q</button>
-           <button className='drum-pad' id='heater2' type="button" onClick={handleClick}><audio src={src2} className="clip" id="W"></audio>W</button>
-           <button className="drum-pad" id='heater3' type="button" onClick={handleClick}><audio src={src3} className="clip" id="E"></audio>E</button>
-           <button className='drum-pad' id='heater4' type="button" onClick={handleClick}><audio src={src4} className="clip" id="A"></audio>A</button>
-           <button className='drum-pad' id='clap' type="button"  onClick={handleClick}><audio src={src5} className="clip" id="S"></audio>S</button>
-           <button className='drum-pad' id='open-HH' type="button" onClick={handleClick}><audio src={src6} className="clip" id="D"></audio>D</button>
-           <button className='drum-pad' id='kick-n-hat' type="button" onClick={handleClick}><audio src={src7}className="clip" id="Z"></audio>Z</button>
-           <button className='drum-pad' id='kick' type="button" onClick={handleClick}><audio src={src8} className="clip" id="X"></audio>X</button>
-           <button className='drum-pad' id='closed-HH' type="button" onClick={handleClick}><audio src={src9} className="clip" id="C"></audio>C</button>
+           <button className='drum-pad' id='Heater 1' type="button" onClick={(e) => handleClick(e,"Q")}><audio src={src1} className="clip" id="Q"></audio>Q</button>
+           <button className='drum-pad' id='Heater 2' type="button" onClick={(e) => handleClick(e, "W")}><audio src={src2} className="clip" id="W"></audio>W</button>
+           <button className="drum-pad" id='Heater 3' type="button" onClick={(e) => handleClick(e, "E")}><audio src={src3} className="clip" id="E"></audio>E</button>
+           <button className='drum-pad' id='Heater 4' type="button" onClick={(e) => handleClick(e, "A")}><audio src={src4} className="clip" id="A"></audio>A</button>
+           <button className='drum-pad' id='Clap' type="button"  onClick={(e) => handleClick(e, "S")}><audio src={src5} className="clip" id="S"></audio>S</button>
+           <button className='drum-pad' id='Open HH' type="button" onClick={(e) => handleClick(e, "D")}><audio src={src6} className="clip" id="D"></audio>D</button>
+           <button className='drum-pad' id='Kick n Hat' type="button" onClick={(e) => handleClick(e, "Z")}><audio src={src7}className="clip" id="Z"></audio>Z</button>
+           <button className='drum-pad' id='Kick' type="button" onClick={(e) => handleClick(e, "X")}><audio src={src8} className="clip" id="X"></audio>X</button>
+           <button className='drum-pad' id='Closed HH' type="button" onClick={(e) => handleClick(e, "C")}><audio src={src9} className="clip" id="C"></audio>C</button>
          </div>
       </div> 
+      <p id="title">Drum Machine By Alex Miserandino</p>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
